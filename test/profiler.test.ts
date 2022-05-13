c0c2fa6a9f463c0c9425b06b2f7e927bf77c7238
Antony Shaleynikov <shaleynikov@gmail.com>
2022-05-13 15:13:38 +0300

    test: Added tests to cover 80% of statements in library

--- test/profiler.test.ts
+++ test/profiler.test.ts
@@ -1,8 +1,11 @@
 import Pyroscope from '@pyroscope/nodejs'
-jest.setTimeout(11000)
-describe('common behavour of profilers', () => {
-    it('should require server name and app name as options', (done) => {
+import express, { application } from 'express'
 
+jest.setTimeout(15000)
+describe('common behavour of profilers', () => {
+
+
+    it('should require server name and app name as options', (done) => {
         Pyroscope.init({})
         expect(Pyroscope.start).toThrowError("Pyroscope is not configured. Please call init() first.");
         
@@ -12,10 +15,42 @@ describe('common behavour of profilers', () => {
         Pyroscope.init({serverAddress: "http://pyroscope:4040", appName: "nodejs"})
         expect(Pyroscope.start).not.toThrowError("Pyroscope is not configured. Please call init() first.")
         Pyroscope.stop()
-
-        // Wait for 10 seconds until exit
-        setTimeout(() => {
+        process.nextTick(() => {
             done();
-        }, 10200);
+        });
     })
+
+    it('should call a server on startCpuProfiling and clear gracefully', (done) => {
+        Pyroscope.init({serverAddress: "http://localhost:4040", appName: "nodejs"})
+        const app = express();
+        const server = app.listen(4040, () => {
+            Pyroscope.startCpuProfiling()
+
+        });
+        app.post('/ingest', (req, res) => {
+            Pyroscope.stopCpuProfiling()
+            expect(req.query['spyName']).toEqual('nodespy');
+            expect(req.query['name']).toEqual('nodejs{}');
+
+            res.send("ok")
+            server.close(done);
+        });
+    });
+
+    it('should call a server on startHeapProfiling and clear gracefully', (done) => {
+        Pyroscope.init({serverAddress: "http://localhost:4444", appName: "nodejs", tags: {env: "test env"}})
+        const app = express();
+        const server = app.listen(4444, () => {
+            Pyroscope.startHeapProfiling()
+        });
+        app.post('/ingest', (req, res) => {
+            Pyroscope.stopHeapProfiling()
+            expect(req.query['spyName']).toEqual('nodespy');
+            expect(req.query['name']).toEqual('nodejs{env=test env}');
+
+            res.send("ok")
+            server.close(done);
+        });
+    });
+
 });
