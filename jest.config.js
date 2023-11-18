6fc0e420cbc77bf7cc32f378a5882fe5ced31d26
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-11-18 20:10:08 +0100

    chore: remove unused module name mapper config

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,21 @@
 module.exports = {
-  "roots": [
-    "src",
-    "test"
-  ],
-  moduleNameMapper: {
-    '^@pyroscope/nodejs$': '<rootDir>/src',
-      "./express.js": "<rootDir>/src/express.ts",
-      "./cpu.js": "<rootDir>/src/cpu.ts",
-      "./wall.js": "<rootDir>/src/wall.ts",
-      "./heap.js": "<rootDir>/src/heap.ts",
-      "./index.js": "<rootDir>/src/index.ts"
-  },
-  preset: "ts-jest",
-  testEnvironment: "node",
+  roots: ['src', 'test'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "test/**/*.{ts,js}",    
-    "!**/node_modules/**",
-    "!**/vendor/**"
+    'src/**/*.{ts,tsx}',
+    'test/**/*.{ts,js}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
   ],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
   globals: {
     'ts-jest': {
       tsconfig: './config/tsconfig.jest.json',
     },
   },
-  "extensionsToTreatAsEsm": [".ts"]
+  extensionsToTreatAsEsm: ['.ts'],
 }
