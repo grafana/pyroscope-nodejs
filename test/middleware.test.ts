5544da41137c2ec594ea475d0f187884c4b7ecb3
Dmitry Filimonov <dmitry@pyroscope.io>
2024-04-30 12:17:10 -0700

    Fixes formatting issues in README.md and test files

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -1,4 +1,4 @@
-import Pyroscope, { expressMiddleware } from '../src'
+import Pyroscope from '../src'
 import request from 'supertest'
 import express from 'express'
 
@@ -10,11 +10,11 @@ describe('express middleware', () => {
     await new Promise<void>((resolve) => setTimeout(() => resolve(), 500)) // avoid jest open handle error
   })
   it('should be a function', () => {
-    expect(typeof expressMiddleware).toBe('function')
+    expect(typeof Pyroscope.expressMiddleware).toBe('function')
   })
   it('should respond to cpu calls', async () => {
     const app = express()
-    app.use(expressMiddleware())
+    app.use(Pyroscope.expressMiddleware())
     return request(app)
       .get('/debug/pprof/profile?seconds=1')
       .then((result) => {
@@ -26,7 +26,7 @@ describe('express middleware', () => {
   })
   it('should respond to repetitive cpu calls', async () => {
     const app = express()
-    app.use(expressMiddleware())
+    app.use(Pyroscope.expressMiddleware())
     return request(app)
       .get('/debug/pprof/profile?seconds=1')
       .then((result) => {
@@ -39,7 +39,7 @@ describe('express middleware', () => {
 
   // it('should respond to simultaneous cpu calls', () => {
   //   const app = express()
-  //   app.use(expressMiddleware())
+  //   app.use(Pyroscope.expressMiddleware())
   //   console.log('0', Date.now()/1000);
   //   return Promise.all([
   //     request(app)
@@ -62,7 +62,7 @@ describe('express middleware', () => {
   // })
   it('should respond to heap profiling calls', () => {
     const app = express()
-    app.use(expressMiddleware())
+    app.use(Pyroscope.expressMiddleware())
     return request(app)
       .get('/debug/pprof/heap')
       .then((result) => expect(result.statusCode).toBe(200))
@@ -72,7 +72,7 @@ describe('express middleware', () => {
   })
   it('should respond to repetitive heap profiling calls', () => {
     const app = express()
-    app.use(expressMiddleware())
+    app.use(Pyroscope.expressMiddleware())
     return request(app)
       .get('/debug/pprof/heap')
       .then((result) => expect(result.statusCode).toBe(200))
@@ -83,7 +83,7 @@ describe('express middleware', () => {
 
   it('should respond to simultaneous heap profiling calls', () => {
     const app = express()
-    app.use(expressMiddleware())
+    app.use(Pyroscope.expressMiddleware())
     return Promise.all([
       request(app)
         .get('/debug/pprof/heap?seconds=1')
@@ -102,10 +102,10 @@ describe('express middleware', () => {
 
   it('should be fine using two middlewares at the same time', () => {
     const app = express()
-    app.use(expressMiddleware())
+    app.use(Pyroscope.expressMiddleware())
 
     const app2 = express()
-    app2.use(expressMiddleware())
+    app2.use(Pyroscope.expressMiddleware())
 
     request(app)
       .get('/debug/pprof/heap')
