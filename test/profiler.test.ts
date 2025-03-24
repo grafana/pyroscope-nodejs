grafana/pyroscope-nodejs

commit 47c118742c1cf634f4c577893e0783cfc4f89d45

chore: drop dependencies on axios and form-data (#112)

src/pyroscope-api-exporter.ts
@@ -1,8 +1,6 @@
 import { URL } from 'node:url';
 
 import { encode } from '@datadog/pprof';
-import axios, { AxiosBasicCredentials, AxiosError } from 'axios';
-import FormData, { Headers } from 'form-data';
 import { Profile } from 'pprof-format';
 import { ProfileExport, ProfileExporter } from './profile-exporter.js';
 import { dateToUnixTimestamp } from './utils/date-to-unix-timestamp.js';
@@ -58,15 +56,22 @@ export class PyroscopeApiExporter implements ProfileExporter {
     return endpointUrl;
   }
 
-  private buildRequestHeaders(formData: FormData): Headers {
-    const headers: Headers = formData.getHeaders();
+  private buildRequestHeaders(): Headers {
+    const headers: Headers = new Headers();
 
     if (this.authToken !== undefined) {
-      headers['authorization'] = `Bearer ${this.authToken}`;
+      headers.set('authorization', `Bearer ${this.authToken}`);
+    } else if (this.config.basicAuthUser && this.config.basicAuthPassword) {
+      headers.set(
+        'authorization',
+        Buffer.from(
+          `${this.config.basicAuthUser}:${this.config.basicAuthPassword}`
+        ).toString('base64')
+      );
     }
 
     if (this.config.tenantID) {
-      headers['X-Scope-OrgID'] = this.config.tenantID;
+      headers.set('X-Scope-OrgID', this.config.tenantID);
     }
 
     return headers;
@@ -81,30 +86,9 @@ export class PyroscopeApiExporter implements ProfileExporter {
 
     const formData: FormData = new FormData();
 
-    formData.append('profile', profileBuffer, {
-      contentType: 'text/json',
-      filename: 'profile',
-      knownLength: profileBuffer.byteLength,
-    });
+    formData.append('profile', new Blob([profileBuffer]), 'profile');
 
     return formData;
-  }
-
-  private handleAxiosError(error: AxiosError): void {
-    if (error.response !== undefined) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      log('Pyroscope received error while ingesting data to server');
-      log(error.response.data);
-    } else if (error.request !== undefined) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      log('Error when ingesting data to server:', error.message);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      log('Error', error.message);
-    }
   }
 
   private async uploadProfile(profileExport: ProfileExport): Promise<void> {
@@ -112,23 +96,26 @@ export class PyroscopeApiExporter implements ProfileExporter {
       profileExport.profile
     );
 
-    const auth: AxiosBasicCredentials | undefined =
-      this.config.basicAuthUser && this.config.basicAuthPassword
-        ? {
-            username: this.config.basicAuthUser,
-            password: this.config.basicAuthPassword,
-          }
-        : undefined;
-
     try {
-      await axios(this.buildEndpointUrl(profileExport).toString(), {
-        data: formData,
-        headers: this.buildRequestHeaders(formData),
-        method: 'POST',
-        auth: auth,
-      });
+      const response = await fetch(
+        this.buildEndpointUrl(profileExport).toString(),
+        {
+          body: formData,
+          headers: this.buildRequestHeaders(),
+          method: 'POST',
+        }
+      );
+
+      if (!response.ok) {
+        log('Server rejected data ingest: HTTP %d', response.status);
+        log(await response.text());
+      }
     } catch (error: unknown) {
-      this.handleAxiosError(error as AxiosError);
+      if (error instanceof Error) {
+        log('Error sending data ingest: %s', error.message);
+      } else {
+        log('Unknown error sending data ingest');
+      }
     }
   }
 }

commit 116d473d5d9b56b4f582185695636561a5c35adb

fix: Basic auth (was broken in v0.4.4) and test were not running (#115)
* Fix basic auth

Basic auth was broken by #112

* Fixing tests

Unfortunately the tests were broken, when we migrated to vitest, as the
(done), callback is no longer supported in there.

I have refactored those a fair bit and it should be all working again.

* Update test/profiler.test.ts

Co-authored-by: Tolya Korniltsev <korniltsev.anatoly@gmail.com>

* Cleanup at test finish

---------

Co-authored-by: Tolya Korniltsev <korniltsev.anatoly@gmail.com>

src/index.ts
@@ -1,19 +1,19 @@
 import 'regenerator-runtime/runtime.js';
 
+import { setLogger as datadogSetLogger } from '@datadog/pprof';
 import expressMiddleware from './express/middleware.js';
+import { Logger, setLogger as ourSetLogger } from './logger.js';
 import { PyroscopeProfiler } from './profilers/pyroscope-profiler.js';
 import {
   PyroscopeConfig,
   PyroscopeHeapConfig,
   PyroscopeWallConfig,
 } from './pyroscope-config.js';
-import { checkPyroscopeConfig } from './utils/check-pyroscope-config.js';
-import { getProfiler, setProfiler } from './utils/pyroscope-profiler.js';
-import { processConfig } from './utils/process-config.js';
-import { getEnv } from './utils/get-env.js';
-import { setLogger as datadogSetLogger } from '@datadog/pprof';
-import { setLogger as ourSetLogger, Logger } from './logger.js';
 import { SourceMapper } from './sourcemapper.js';
+import { checkPyroscopeConfig } from './utils/check-pyroscope-config.js';
+import { getEnv } from './utils/get-env.js';
+import { processConfig } from './utils/process-config.js';
+import { getProfiler, setProfiler } from './utils/pyroscope-profiler.js';
 
 export function init(config: PyroscopeConfig = {}): void {
   checkPyroscopeConfig(config);

src/pyroscope-api-exporter.ts
@@ -64,9 +64,9 @@ export class PyroscopeApiExporter implements ProfileExporter {
     } else if (this.config.basicAuthUser && this.config.basicAuthPassword) {
       headers.set(
         'authorization',
-        Buffer.from(
+        `Basic ${Buffer.from(
           `${this.config.basicAuthUser}:${this.config.basicAuthPassword}`
-        ).toString('base64')
+        ).toString('base64')}`
       );
     }
 

test/profiler.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, onTestFinished } from 'vitest';
 
 import Pyroscope from '../src/index.js';
 import express from 'express';
@@ -6,6 +6,26 @@ import busboy from 'busboy';
 import { Profile } from 'pprof-format';
 import zlib from 'zlib';
 
+// createBackend creates an Express server with an /ingest endpoint and returns a promise
+// that resolves to the HTTP port the server is listening on
+const createBackend = (handler: express.RequestHandler): Promise<number> => {
+  const server = express();
+  const port = new Promise<number>((resolvePort, rejectPort) => {
+    const httpServer = server.listen(0, () => {
+      const address = httpServer.address();
+      if (address && typeof address === 'object') {
+        resolvePort(address.port);
+      } else {
+        rejectPort('Could not resolve port');
+      }
+    });
+    onTestFinished(() => {
+      httpServer.close();
+    });
+  });
+  server.post('/ingest', handler);
+  return port;
+};
 type Numeric = number | bigint;
 
 const extractProfile = (
@@ -36,84 +56,76 @@ const doWork = (d: number): void => {
 };
 
 describe('common behaviour of profilers', () => {
-  it('should call a server on startCpuProfiling and clear gracefully', (done) => {
-    Pyroscope.init({
-      serverAddress: 'http://localhost:4445',
-      appName: 'nodejs',
-      flushIntervalMs: 100,
-      wall: {
-        samplingDurationMs: 1000,
-      },
-    });
-    const app = express();
-    const server = app.listen(4445, () => {
-      Pyroscope.startWallProfiling();
+  it('should call a server on startCpuProfiling and clear gracefully', async () => {
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          resolve(req);
+          res.send('ok');
+        }
+      );
+
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          wall: {
+            samplingDurationMs: 1000,
+          },
+        });
+        Pyroscope.startWallProfiling();
+        doWork(0.1);
+      });
     });
 
-    let closeInvoked = false;
-
-    app.post('/ingest', (req, res) => {
-      expect(req.query['spyName']).toEqual('nodespy');
-      expect(req.query['name']).toEqual('nodejs{}');
-      res.send('ok');
-      if (!closeInvoked) {
-        closeInvoked = true;
-        (async () => {
-          await Pyroscope.stopWallProfiling();
-          server.close(() => {
-            done();
-          });
-        })();
-      }
-    });
+    const req = await firstRequest;
+    await Pyroscope.stopWallProfiling();
+    expect(req.query.spyName).toBe('nodespy');
+    expect(req.query.name).toBe('nodejs{}');
   });
 
-  it('should call a server on startHeapProfiling and clear gracefully', (done) => {
-    // Simulate memory usage
-    const timer: NodeJS.Timeout = setInterval(() => {
-      // Use some memory
-      new Array<number>(2000)
-        .fill(3)
-        .reduce(
-          (previous: number, current: number): number => previous + current,
-          0
-        );
-    }, 100);
+  it('should call a server on startHeapProfiling and clear gracefully', async () => {
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          resolve(req);
+          res.send('ok');
+        }
+      );
 
-    Pyroscope.init({
-      serverAddress: 'http://localhost:4444',
-      appName: 'nodejs',
-      flushIntervalMs: 100,
-      tags: { env: 'test env' },
-      heap: {
-        samplingIntervalBytes: 1024,
-      },
-      wall: {
-        samplingDurationMs: 1000,
-      },
-    });
-    const app = express();
-    const server = app.listen(4444, () => {
-      Pyroscope.startHeapProfiling();
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          tags: { env: 'test env' },
+          heap: {
+            samplingIntervalBytes: 1024,
+          },
+          wall: {
+            samplingDurationMs: 1000,
+          },
+        });
+        Pyroscope.startHeapProfiling();
+
+        // Simulate memory usage
+        setInterval(() => {
+          // Use some memory
+          new Array<number>(2000)
+            .fill(3)
+            .reduce(
+              (previous: number, current: number): number => previous + current,
+              0
+            );
+        }, 100);
+      });
     });
 
-    let closeInvoked = false;
-
-    app.post('/ingest', (req, res) => {
-      expect(req.query['spyName']).toEqual('nodespy');
-      expect(req.query['name']).toEqual('nodejs{env=test env}');
-      res.send('ok');
-      if (!closeInvoked) {
-        closeInvoked = true;
-        (async () => {
-          await Pyroscope.stopHeapProfiling();
-          clearInterval(timer);
-          server.close(() => {
-            done();
-          });
-        })();
-      }
-    });
+    const req = await firstRequest;
+    await Pyroscope.stopHeapProfiling();
+    expect(req.query['spyName']).toBe('nodespy');
+    expect(req.query['name']).toBe('nodejs{env=test env}');
   });
 
   it('should allow to call start profiling twice', async () => {
@@ -150,144 +162,227 @@ describe('common behaviour of profilers', () => {
     clearInterval(timer);
   });
 
-  it('should have dynamic labels on wall profile', (done) => {
-    Pyroscope.init({
-      serverAddress: 'http://localhost:4446',
-      appName: 'nodejs',
-      flushIntervalMs: 100,
-      heap: {
-        samplingIntervalBytes: 1000,
-      },
-      wall: {
-        samplingDurationMs: 1000,
-        samplingIntervalMicros: 100,
-      },
-    });
-    const app = express();
-    const server = app.listen(4446, () => {
-      Pyroscope.startWallProfiling();
-      Pyroscope.wrapWithLabels(
-        {
-          vehicle: 'car',
-        },
-        () => {
-          doWork(0.2);
-          Pyroscope.wrapWithLabels(
-            {
-              brand: 'mercedes',
-            },
-            () => {
-              doWork(0.2);
-            }
-          );
+  it('should have dynamic labels on wall profile', async () => {
+    const valuesPerLabel = new Map<string, Array<number>>();
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          extractProfile(req, res, (p: Profile) => {
+            const s = (idx: Numeric): string =>
+              p.stringTable.strings[Number(idx)];
+
+            // aggregate per labels
+            p.sample.forEach((x) => {
+              const key: string = JSON.stringify(
+                x.label.reduce(
+                  (result, current) => ({
+                    ...result,
+                    [s(current.key)]: s(current.str),
+                  }),
+                  {}
+                )
+              );
+              const prev = valuesPerLabel.get(key) ?? [0, 0, 0];
+              valuesPerLabel.set(
+                key,
+                x.value.map((a, i) => Number(a) + prev[i])
+              );
+            });
+          });
+          resolve(req);
         }
       );
-    });
-    let closeInvoked = false;
-    const valuesPerLabel = new Map<string, Array<number>>();
 
-    app.post('/ingest', (req, res) => {
-      expect(req.query['spyName']).toEqual('nodespy');
-      expect(req.query['name']).toEqual('nodejs{}');
-      extractProfile(req, res, (p: Profile) => {
-        const s = (idx: Numeric): string => p.stringTable.strings[Number(idx)];
-
-        // aggregate per labels
-        p.sample.forEach((x) => {
-          const key: string = JSON.stringify(
-            x.label.reduce(
-              (result, current) => ({
-                ...result,
-                [s(current.key)]: s(current.str),
-              }),
-              {}
-            )
-          );
-          const prev = valuesPerLabel.get(key) ?? [0, 0, 0];
-          valuesPerLabel.set(
-            key,
-            x.value.map((a, i) => Number(a) + prev[i])
-          );
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          heap: {
+            samplingIntervalBytes: 1000,
+          },
+          wall: {
+            samplingDurationMs: 1000,
+            samplingIntervalMicros: 1000,
+          },
         });
+        Pyroscope.startWallProfiling();
+        Pyroscope.wrapWithLabels(
+          {
+            vehicle: 'car',
+          },
+          () => {
+            doWork(0.1);
+            Pyroscope.wrapWithLabels(
+              {
+                brand: 'mercedes',
+              },
+              () => {
+                doWork(0.1);
+              }
+            );
+          }
+        );
       });
-
-      if (!closeInvoked) {
-        closeInvoked = true;
-        (async () => {
-          await Pyroscope.stopWallProfiling();
-          server.close(() => {
-            // ensure we contain everything expected
-            const emptyLabels = JSON.stringify({});
-            const vehicleOnly = JSON.stringify({ vehicle: 'car' });
-            const vehicleAndBrand = JSON.stringify({
-              vehicle: 'car',
-              brand: 'mercedes',
-            });
-
-            expect(valuesPerLabel.keys()).toContain(emptyLabels);
-            expect(valuesPerLabel.keys()).toContain(vehicleOnly);
-            expect(valuesPerLabel.keys()).toContain(vehicleAndBrand);
-
-            const valuesVehicleOnly = valuesPerLabel.get(vehicleOnly) ?? [0, 0];
-            const valuesVehicleAndBrand = valuesPerLabel.get(
-              vehicleAndBrand
-            ) ?? [0, 0];
-
-            // ensure the wall time is within a 20% range of each other
-            const ratio = valuesVehicleOnly[1] / valuesVehicleAndBrand[1];
-            expect(ratio).toBeGreaterThan(0.8);
-            expect(ratio).toBeLessThan(1.2);
-
-            done();
-          });
-        })();
-      }
     });
+
+    const req = await firstRequest;
+    await Pyroscope.stopWallProfiling();
+
+    expect(req.query['spyName']).toEqual('nodespy');
+    expect(req.query['name']).toEqual('nodejs{}');
+
+    // ensure we contain everything expected
+    const emptyLabels = JSON.stringify({});
+    const vehicleOnly = JSON.stringify({ vehicle: 'car' });
+    const vehicleAndBrand = JSON.stringify({
+      vehicle: 'car',
+      brand: 'mercedes',
+    });
+
+    expect(valuesPerLabel.keys()).toContain(emptyLabels);
+    expect(valuesPerLabel.keys()).toContain(vehicleOnly);
+    expect(valuesPerLabel.keys()).toContain(vehicleAndBrand);
+
+    const valuesVehicleOnly = valuesPerLabel.get(vehicleOnly) ?? [0, 0];
+    const valuesVehicleAndBrand = valuesPerLabel.get(vehicleAndBrand) ?? [0, 0];
+
+    // ensure the wall time is within a 20% range of each other
+    const ratio = valuesVehicleOnly[1] / valuesVehicleAndBrand[1];
+    expect(ratio).toBeGreaterThan(0.8);
+    expect(ratio).toBeLessThan(1.2);
   });
 
-  it('should have extra samples for cpu time when enabled on wall profile', (done) => {
-    Pyroscope.init({
-      serverAddress: 'http://localhost:4447',
-      appName: 'nodejs',
-      flushIntervalMs: 100,
-      heap: {
-        samplingIntervalBytes: 1000,
-      },
-      wall: {
-        samplingDurationMs: 1000,
-        samplingIntervalMicros: 100,
-        collectCpuTime: true,
-      },
-    });
-    const app = express();
-    const server = app.listen(4447, () => {
-      Pyroscope.startWallProfiling();
-    });
-
-    let closeInvoked = false;
-    app.post('/ingest', (req, res) => {
-      expect(req.query['spyName']).toEqual('nodespy');
-      expect(req.query['name']).toEqual('nodejs{}');
-
-      extractProfile(req, res, (p: Profile) => {
-        const s = (idx: number | bigint) => p.stringTable.strings[Number(idx)];
-        // expect sample, wall and cpu types
-        expect(p.sampleType.map((x) => `${s(x.type)}=${s(x.unit)}`)).toEqual([
-          'samples=count',
-          'wall=nanoseconds',
-          'cpu=nanoseconds',
-        ]);
-      });
-
-      if (!closeInvoked) {
-        closeInvoked = true;
-        (async () => {
-          await Pyroscope.stopWallProfiling();
-          server.close(() => {
-            done();
+  it('should have extra samples for cpu time when enabled on wall profile', async () => {
+    let sampleType: string[] = [];
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          extractProfile(req, res, (p: Profile) => {
+            const s = (idx: number | bigint) =>
+              p.stringTable.strings[Number(idx)];
+            sampleType = p.sampleType.map((x) => `${s(x.type)}=${s(x.unit)}`);
           });
-        })();
-      }
+          resolve(req);
+        }
+      );
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          heap: {
+            samplingIntervalBytes: 1000,
+          },
+          wall: {
+            samplingDurationMs: 1000,
+            samplingIntervalMicros: 1000,
+            collectCpuTime: true,
+          },
+        });
+        Pyroscope.startWallProfiling();
+        doWork(0.1);
+      });
     });
+
+    const req = await firstRequest;
+    await Pyroscope.stopWallProfiling();
+
+    expect(req.query['spyName']).toEqual('nodespy');
+    expect(req.query['name']).toEqual('nodejs{}');
+    // expect sample, wall and cpu types
+    expect(sampleType).toEqual([
+      'samples=count',
+      'wall=nanoseconds',
+      'cpu=nanoseconds',
+    ]);
+  });
+
+  it('should send bearer authentication header when configured', async () => {
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          resolve(req);
+          res.send('ok');
+        }
+      );
+
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          wall: {
+            samplingDurationMs: 1000,
+          },
+          authToken: 'my-token',
+        });
+        Pyroscope.startWallProfiling();
+        doWork(0.01);
+      });
+    });
+
+    const req = await firstRequest;
+    await Pyroscope.stopWallProfiling();
+    expect(req.headers['authorization']).toBe('Bearer my-token');
+  });
+
+  it('should send basic authentication header when configured', async () => {
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          resolve(req);
+          res.send('ok');
+        }
+      );
+
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          wall: {
+            samplingDurationMs: 1000,
+          },
+          basicAuthUser: 'user',
+          basicAuthPassword: 'password',
+        });
+        Pyroscope.startWallProfiling();
+        doWork(0.01);
+      });
+    });
+
+    const req = await firstRequest;
+    await Pyroscope.stopWallProfiling();
+    expect(req.headers['authorization']).toBe('Basic dXNlcjpwYXNzd29yZA==');
+  });
+
+  it('should send x-scope-orgid header when configured', async () => {
+    const firstRequest = new Promise<express.Request>((resolve) => {
+      const port = createBackend(
+        (req: express.Request, res: express.Response) => {
+          resolve(req);
+          res.send('ok');
+        }
+      );
+
+      port.then((p: number) => {
+        Pyroscope.init({
+          serverAddress: `http://localhost:${p}`,
+          appName: 'nodejs',
+          flushIntervalMs: 100,
+          wall: {
+            samplingDurationMs: 1000,
+          },
+          tenantID: 'my-tenant-id',
+        });
+        Pyroscope.startWallProfiling();
+        doWork(0.01);
+      });
+    });
+
+    const req = await firstRequest;
+    await Pyroscope.stopWallProfiling();
+    expect(req.headers['x-scope-orgid']).toBe('my-tenant-id');
   });
 });